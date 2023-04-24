6607f5effbbb71c24c862a904c04ebf08001f29f
Bronson Yen <bronson1013@gmail.com>
2023-04-24 14:11:27 -0400

    Rename download_image

--- src/download_image.c
+++ src/download_image.c
@@ -1,4 +1,4 @@
-#include "protest.h"
+#include "download_image.h"
 
 size_t write_data(void *ptr, size_t size, size_t nmemb, void *stream) {
 	size_t written = fwrite(ptr, size, nmemb, (FILE *)stream);
