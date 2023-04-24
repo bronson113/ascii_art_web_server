823b4dc101f93909ec05a9826b458289a782ece2
Bronson Yen <bronson1013@gmail.com>
2023-04-24 15:46:54 -0400

    Connect download file with ascii art convertor

---
+++ inc/ascii_conversion.h
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/*
+credit to bitesofcode.com for the concept and ChatGPT for the generation of the code. Comments are utilized here to indicate
+understanding of the code
+*/
+
+//defines a structure to store the RGB values for the bitmap 
+typedef struct
+{
+    int R;
+    int G;
+    int B;
+} Color;
+
+//defines a structure that defines a Bitmap object and different objects within 
+typedef struct
+{
+    int Width;
+    int Height;
+    Color** Pixels;
+} Bitmap;
+
+typedef struct
+{
+    int Width;
+    int Height;
+    char** Pixels;
+} Asciimap;
+
+Bitmap Grayscale(Bitmap image);
+Asciimap Convert_to_ascii(Bitmap image);
